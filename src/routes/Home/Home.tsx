ae65beb06855c2e3a2a579bbe19565021fe803e4
Fernando Lopes <fernandolopes_38@hotmail.com>
2022-11-23 22:15:50 -0300

    added global styles and router

---
+++ src/routes/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+interface HomeProps {}
+
+export const Home: React.FC<HomeProps> = ({}) => {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+};
